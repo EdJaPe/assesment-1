509038256b19494b3a7ed654807c0cc524db4fea
edgard Jara <edv06jara@gmail.com>
2021-01-07 12:13:06 -0800

    Assestment 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var form = document.querySelector("form")
+var resultado = document.querySelector("#result")
+
+form.addEventListener("click", function(e)){
+    e.preventDefault()
+    var numero = form.elements["input"].value;
+    
+
+//create and if statement that adds or subtracts input depending what is pressed
+//display result in var (resultado) 
+
