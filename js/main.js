8bf5de154f5c5d42395091aee11c241ee2299d2d
edgard Jara <edv06jara@gmail.com>
2021-01-07 18:40:24 -0800

    finished product

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-var form = document.querySelector("form")
-var resultado = document.querySelector("#result")
 
-form.addEventListener("click", function(e)){
-    e.preventDefault()
-    var numero = form.elements["input"].value;
+const result = document.getElementById("result")
+const input = document.getElementById("numero")
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+
+let num = 0;
+function addNum (e) {
+    e.preventDefault();
+    num += parseInt(input.value);
+    result.innerText = num;
+    if (num >= 0){
+        result.style.color ="black";
     
-
+    }
+}
+function minusNum (e) {
+    e.preventDefault();
+    num -= parseInt(input.value);
+    result.innerText = num;
+    if (num < 0){
+        result.style.color ="red";
+    
+    }
+}
+plus.addEventListener("click", (e) => addNum(e))
+minus.addEventListener("click", (e) => minusNum(e))
 //create and if statement that adds or subtracts input depending what is pressed
 //display result in var (resultado) 
 
